820af8345bbca4bcb4bbf983d778654108eb04ee
ravagefox <44389726+ravagefox@users.noreply.github.com>
2025-08-25 15:24:34 +1000

    Create runtime.py
    Easier runtime access setup

---
+++ compiler/runtime.py
@@ -0,0 +1,8 @@
+
+"""Define the platform runtime handlers here"""
+def createRuntimeStd():
+  return {
+    ('std', 'print'):   lambda s: print(s, end=''),
+    ('std', 'println'): lambda s: print(s),
+    ('std', 'toArray'): lambda s: list(s),
+  }
